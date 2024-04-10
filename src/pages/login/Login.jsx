07ef64787cbf7149f560904986cf164d8225218c
Amir-Thp <code2time@gmail.com>
2024-04-10 22:25:31 +0330

    create Login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import './Login.css'
+
 
 function Login() {
   return (
-    <div>
+    <div className='login-container'>
      <Navbar />
-     login page 
+     <div className="wrapper mx-auto my-[200px] ">
+        <form action="">
+            <h1 className='Lalezar'>ورود/ایجاد حساب</h1>
+            <div className="input-box">
+                <input  className='Lamia' type="text" placeholder="نام کاربری" required/>
+                
+                <i className='bx bxs-user'></i>
+            </div>
+            <div className="input-box">
+                <input  className='Lamia' type="password" placeholder="رمز عبور" required/>
+                <i className='bx bxs-lock-alt' ></i>
+            </div>
+              <button type="button"  className="btn Lamia" >ورود</button>
+           
+        </form>
+    </div>
     </div>
   )
 }
